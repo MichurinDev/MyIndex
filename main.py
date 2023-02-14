6642f23bb56bc5e5fc5ccd6852e696654c6a80b0
MichurinDev <420520052006MfDeveloper>
2023-02-15 04:29:12 +0700

    Реализована механика регистрации и авторизации

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Пример кода для получения информации по адресу
 from get_post_info_by_address import GetPostInfoByAddress
+from registration import Registration, Authentication
 
 HomeAddress = input("Введите адрес: ")
 PostOfficeIndex, PostOfficeAddress = GetPostInfoByAddress(HomeAddress)
@@ -7,3 +8,6 @@ PostOfficeIndex, PostOfficeAddress = GetPostInfoByAddress(HomeAddress)
 print(f"""Введённый адрес: {HomeAddress}
 Индекс: {PostOfficeIndex}
 Адрес ОПС: {PostOfficeAddress}""")
+
+print(Registration("./Data/user_data.db", "Andrew", "MichurinDev", "123"))
+print(Authentication("./Data/user_data.db", "MichurinDev", "123"))
